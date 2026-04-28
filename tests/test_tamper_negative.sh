52f70f24461a4996b9e1222d71c61564205cac06
Huyentrang03789 <duongthtrang06@gmail.com>
2026-04-28 16:57:21 +0700

    Update test_tamper_negative.sh

--- tests/test_tamper_negative.sh
+++ tests/test_tamper_negative.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-# Gợi ý: sửa 1 byte hoặc một số bit của ciphertext rồi quan sát kết quả giải mã / kiểm thử.
+# Test: sửa 1 byte hoặc một số bit của ciphertext rồi quan sát kết quả giải mã / kiểm thử.
 set -euo pipefail
 
 g++ -std=c++17 des.cpp -o des
